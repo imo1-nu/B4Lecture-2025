e51b6f2d24ec10ca20479550bce7c65d782849f6
Leftie <kaselias@yahoo.gr>
2025-05-09 17:12:12 +0900

    Scatterplots

--- ex3/i_tzimas/kaiki.py
+++ ex3/i_tzimas/kaiki.py
@@ -1,7 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Scatterplots
 data1 = np.loadtxt("ex3/data/data1.csv", skiprows=1, delimiter=",")
 
-plt.scatter(data1[:, 0], data1[:, 1])
+plt.scatter(data1[:, 0], data1[:, 1], 20, "#912583", zorder=2)
+plt.title("Data 1 Scatterplot")
+plt.grid()
 plt.savefig("ex3/i_tzimas/data1_scatter.png")
+plt.clf()
+
+data2 = np.loadtxt("ex3/data/data2.csv", skiprows=1, delimiter=",")
+
+plt.scatter(data2[:, 0], data2[:, 1], 20, "#912583", zorder=2)
+plt.title("Data 2 Scatterplot")
+plt.grid()
+plt.savefig("ex3/i_tzimas/data2_scatter.png")
+plt.clf()
+
+data3 = np.loadtxt("ex3/data/data3.csv", skiprows=1, delimiter=",")
+plt.subplot(111, projection="3d").scatter(data3[:, 0], data3[:, 1], data3[:, 2])
+plt.title("Data 3 Scatterplot")
+# plt.show()
